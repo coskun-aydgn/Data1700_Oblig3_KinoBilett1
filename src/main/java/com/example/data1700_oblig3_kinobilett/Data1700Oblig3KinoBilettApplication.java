1b2095b32f81f4d25ecf394cefb41d76ceb4fbbf
47462 <coskun.aydn@gmail.com>
2024-04-01 00:27:54 +0200

    Initial commit

---
+++ src/main/java/com/example/data1700_oblig3_kinobilett/Data1700Oblig3KinoBilettApplication.java
@@ -0,0 +1,13 @@
+package com.example.data1700_oblig3_kinobilett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data1700Oblig3KinoBilettApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Data1700Oblig3KinoBilettApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/data1700_oblig3_kinobilett/Data1700Oblig3KinoBilettApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data1700_oblig3_kinobilett;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1700Oblig3KinoBilettApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
